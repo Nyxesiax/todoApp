8161643d47d07e727efcef217a0cd2024b3cefb6
Nyxesiax <raphael.tamdao@googlemail.com>
2019-09-11 14:59:53 +0200

    Erster Commit

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Todo} from '../../model/Todo';
+import {Categories} from '../../model/Categories';
 
 @Component({
   selector: 'app-list',
@@ -8,9 +9,14 @@ import {Todo} from '../../model/Todo';
 })
 export class ListComponent implements OnInit {
   newTodo: Todo;
+ /* categories: Categories[] = [];
+  arbeit: Categories = new Categories('Arbeit');
+  privat: Categories = new Categories('Privat');
+  hobby: Categories = new Categories('Hobby');
+  */
 todos: Todo[] = [];
   constructor() {
-    this.newTodo = new Todo('', '');
+    this.newTodo = new Todo('');
   }
 
   ngOnInit() {
@@ -18,13 +24,13 @@ todos: Todo[] = [];
 
   toggle(todo: Todo) {
     todo.done = !todo.done;
+    this.todos.sort(x => !x.done ? -1 : 1);
   }
 
   save() {
     this.todos.push(this.newTodo);
-    this.newTodo = new Todo('', '');
+    this.newTodo = new Todo('');
   }
-
 
   delete(todoTask: Todo) {
     this.todos = this.todos.filter(t => t !== todoTask);
--- src/app/model/Todo.ts
+++ src/app/model/Todo.ts
@@ -2,7 +2,7 @@ import {Categories} from './Categories';
 
 export class Todo {
   done: boolean;
-  constructor(public task: string, public category: Categories) {
+  constructor(public task: string) {
     this.done = false;
   }
 }
